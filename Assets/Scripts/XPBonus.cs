0fd0132e918b0f7a2516acbf876c92ab9df2b328
Seblong2 <sebastian2.long@live.uwe.ac.uk>
2025-03-26 19:57:25 +0000

    Bonus XP mechanic start
    just added a basic 3d text that rotates and gives player a set amount of  bonus xp when tapped

---
+++ Assets/Scripts/XPBonus.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XPBonus : MonoBehaviour
+{
+    [SerializeField] private int bonus = 10;
+
+    private void OnMouseDown()
+    {
+        GameManager.Instance.CurrentPlayer.addXP(bonus);
+        Destroy(gameObject);
+    }
+}
