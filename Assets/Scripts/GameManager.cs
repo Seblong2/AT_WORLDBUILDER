5cd1ef10cb9ba37b9d20bc5840c1dd313bab72cc
Seblong2 <sebastian2.long@live.uwe.ac.uk>
2025-03-30 17:04:20 +0100

    Changes to player data due to transitions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,15 +5,16 @@ using UnityEngine.Assertions;
 
 public class GameManager : Singleton<GameManager>
 {
-    [SerializeField] private Player currentPlayer;
+    private Player currentPlayer;
 
     public Player CurrentPlayer 
     { 
-       get { return currentPlayer; } 
+       get {
+            if(currentPlayer == null)
+            {
+                currentPlayer = gameObject.AddComponent<Player>();
+            }
+            return currentPlayer; } 
     }
 
-    private void Awake()
-    {
-        Assert.IsNotNull(currentPlayer); // Making sure a player has been assigned on game start
-    }
 }
